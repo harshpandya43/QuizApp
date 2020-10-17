0d99ee29979fa3c3a28224fd2c6fa7b29902b842
Harsh Pandya <pandyaharsh43@gmail.com>
2020-10-17 00:48:58 -0500

    Updates

---
+++ precache-manifest.1459b7396c3a9f7601fc75b6fe08c8b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45503401480b2284476d436bab2cf7ca",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "685e0b6f88887c09944b",
+    "url": "/QuizApp/static/css/main.709652b3.chunk.css"
+  },
+  {
+    "revision": "34c3fafb4817ce8be14a",
+    "url": "/QuizApp/static/js/2.71dc2452.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/QuizApp/static/js/2.71dc2452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685e0b6f88887c09944b",
+    "url": "/QuizApp/static/js/main.9b27ae33.chunk.js"
+  },
+  {
+    "revision": "43f380b45c9c6e39e41c",
+    "url": "/QuizApp/static/js/runtime-main.a8d7fb93.js"
+  },
+  {
+    "revision": "84bd06ffb80b8144f9c597491ce248a7",
+    "url": "/QuizApp/static/media/Navy Blue Anniversary Card (2).84bd06ff.jpg"
+  }
+]);
